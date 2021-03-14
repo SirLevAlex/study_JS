ff94e8afd4bcb342c672f8736443c785b6bbaf84
SirLevAlex <boss.lary@mail.ru>
2021-03-14 21:13:41 +0400

    Исправио написание кода

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let money, income, addExpenses, deposit, mission, period;
 
 money = 60000;
-income = 'frilance'
-addExpenses = 'Internet, Taxi, Rent, Other';
+income = "frilance";
+addExpenses = "Internet, Taxi, Rent, Other";
 deposit = true;
 mission = 10000;
 period = 6;
@@ -12,9 +12,16 @@ console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев.' +
-  ' Цель заработать ' + mission + ' долларов');
-console.log(addExpenses.toLowerCase(), addExpenses.split(','));
+
+console.log(
+  "Период равен " +
+    period +
+    " месяцев." +
+    " Цель заработать " +
+    mission +
+    " долларов"
+);
+console.log(addExpenses.toLowerCase(), addExpenses.split(","));
 
 let budgetDay = money / 30;
 
